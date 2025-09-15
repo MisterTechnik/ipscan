cba9a42e17d41a2b3b291e5a8dfdc63ea658f2ef
MisterTechnik <finneliasw@gmail.com>
2025-09-16 00:35:44 +0200

    Skip pingAlive test on Windows platform

--- test/net/azib/ipscan/core/net/TCPPingerTest.java
+++ test/net/azib/ipscan/core/net/TCPPingerTest.java
@@ -1,7 +1,20 @@
 package net.azib.ipscan.core.net;
 
+import net.azib.ipscan.config.Platform;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assume.assumeFalse;
+
 public class TCPPingerTest extends AbstractPingerTest {
 	public TCPPingerTest() throws Exception {
 		super(TCPPinger.class);
+	}
+
+	@Test @Override
+	public void pingAlive() throws IOException {
+		assumeFalse(Platform.WINDOWS);
+		super.pingAlive();
 	}
 }
